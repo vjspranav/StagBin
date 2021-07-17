e145f67e99bd134580e342bdffef13e2ae963c9a
vjspranav <pranavasri@live.in>
2021-07-18 00:14:44 +0530

    Remove unused imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Editor from "./components/Editor";
 
 function App() {
