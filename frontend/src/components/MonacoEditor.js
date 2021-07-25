6a450ef9d687515e02c0b450a7bcd6bf1131d821
vjspranav <pranavasri@live.in>
2021-07-25 21:51:28 +0530

    Add support for diff editor

--- frontend/src/components/MonacoEditor.js
+++ frontend/src/components/MonacoEditor.js
@@ -1,16 +1,29 @@
-import Editor from "@monaco-editor/react";
+import Editor, { DiffEditor } from "@monaco-editor/react";
 import { useParams } from "react-router-dom";
 export default function MEditor(props) {
   const curTheme = props.curTheme;
   const { id } = useParams();
 
-  return (
+  const diffEditor = (
+    <DiffEditor
+      height="90vh"
+      defaultLanguage="javascript"
+      original=""
+      modified={id}
+    />
+  );
+
+  const editor = (
     <Editor
       theme={curTheme === "light" ? "light" : "vs-dark"}
       height="88vh"
       defaultLanguage="javascript"
       defaultValue={id ? "content from " + id : "//Enter text"}
       colorDecorators="true"
+      automaticLayout="true"
     />
   );
+  console.log(id);
+
+  return id ? diffEditor : editor;
 }
