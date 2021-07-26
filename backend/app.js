7f38735ba9f98dbd083088dc9b1fc2b2494b6a9b
vjspranav <pranavasri@live.in>
2021-07-27 01:01:58 +0530

    Add support for large files

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+const bodyParser = require("body-parser");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -44,7 +45,8 @@ app.set("view engine", "jade");
 
 app.use(cors());
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
